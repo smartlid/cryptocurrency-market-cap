1bde363baa651c56405b62abfafd019483412283
smartlid <lidavidwork@gmail.com>
2022-07-06 22:18:22 -0400

    Lazy loading components to reduce bundled size

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
-import{BrowserRouter,Route, Routes} from 'react-router-dom'
-import {makeStyles} from '@material-ui/core'
-import Header from './Components/Headers'
-import HomePage from './Pages/HomePage'
-import CoinsPage from './Pages/CoinsPage'
+import React, { lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+import Header from "./Components/Headers";
+const HomePage = lazy(() => import("./Pages/HomePage"));
+const CoinsPage = lazy(() => import("./Pages/CoinsPage"));
 
-const useStyles = makeStyles(()=>({
-  App: {
-    backgroundColor:"#14161a",
-    color:"white",
-    minHeight:"100vh",
-
-  },
+const useStyles = makeStyles(() => ({
+    App: {
+        backgroundColor: "#14161a",
+        color: "white",
+        minHeight: "100vh",
+    },
 }));
 function App() {
+    const classes = useStyles();
 
-const classes = useStyles();
-  
-  return (
-   <BrowserRouter>
-   <div className={classes.App}>
-     <Header/>
-     <Routes>
-
-     <Route path={'/'} element={<HomePage/>} />
-     <Route path={'/coins/:id'} element={<CoinsPage/>}/>
-     </Routes>
-
-   </div>
-   
-   </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <div className={classes.App}>
+                <Header />
+                <Routes>
+                    <Route path={"/"} element={<HomePage />} />
+                    <Route path={"/coins/:id"} element={<CoinsPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
